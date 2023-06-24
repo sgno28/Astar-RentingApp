d3af5d0d74527c511f8636d0bb4fa7c317b59877
Alex Ng <alexanderngyewshen@hmail.com>
2023-06-24 21:56:40 +0100

    styling changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ import backgroundimg from "../assets/mainbg.png";
 const style = {
   bodyContainer: "flex  min-h-screen",
   verticalContainer: "flex flex-col items-start justify-start",
-  h1: "mb-4 text-9xl font-extrabold leading-none tracking-tight text-white md:text-5xl lg:text-6xl mb-8",
+  h1: "mb-4  font-extrabold leading-none tracking-tight text-white md:text-5xl lg:text-6xl mb-8",
   button:
     "text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:outline-none font-medium rounded-lg text-sm px-5 py-2 text-center mr-2 mb-2",
   container: "flex justify-center ml-4",
